a72987765d670265b5eae7049acc9f23e35500e5
Pedroguibas <pedroguibas123@gmail.com>
2026-07-31 12:06:48 -0300

    fix: fixes Uvs on mesh factory

--- src/Texture.cpp
+++ src/Texture.cpp
@@ -6,6 +6,7 @@
 Texture::Texture(const string &path) {
   int channels;
 
+  stbi_set_flip_vertically_on_load(true);
   unsigned char *data = stbi_load(path.c_str(), &width, &height, &channels, 0);
 
   if (!data)
